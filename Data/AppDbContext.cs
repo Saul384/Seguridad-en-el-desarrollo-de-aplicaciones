928ff134d747fbb8720c9712a5107cd301244c6b
Saul384 <2023371155.com>
2026-06-02 16:17:44 -0600

    avance de practica

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using VulnerableApp.Models;
+
+namespace VulnerableApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Creamos una fecha estática para que Entity Framework no marque error
+            var fechaEstatica = new DateTime(2024, 1, 1);
+
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Username = "admin", Password = "admin", Email = "admin@test.com", Balance = 1000m, CreatedAt = fechaEstatica },
+                new User { Id = 2, Username = "user1", Password = "123456", Email = "user@test.com", Balance = 500m, CreatedAt = fechaEstatica },
+                new User { Id = 3, Username = "user2", Password = "password", Email = "user2@test.com", Balance = 750m, CreatedAt = fechaEstatica }
+            );
+        }
+    }
+}
---
+++ Migrations/20260602015731_InitialCreateV2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VulnerableApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 6, 1, 19, 54, 10, 640, DateTimeKind.Local).AddTicks(4037));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 6, 1, 19, 54, 10, 643, DateTimeKind.Local).AddTicks(2461));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 6, 1, 19, 54, 10, 643, DateTimeKind.Local).AddTicks(2485));
+        }
+    }
+}
