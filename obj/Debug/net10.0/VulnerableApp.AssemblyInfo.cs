d151353c84e95eb44c71ea9bcfe8f7731e26dafc
Saul384 <2023371155.com>
2026-06-05 13:44:22 -0600

    avance practica IDOR

--- obj/Debug/net10.0/VulnerableApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/VulnerableApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VulnerableApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798cbd3e5edd22622be61b88afe25874897497cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edfea70eb1b8f54be2bad60ab855a3f429c20f29")]
 [assembly: System.Reflection.AssemblyProductAttribute("VulnerableApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VulnerableApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
